050ff727b8d83d71983bff153e2c5ed994a456d1
nwlh <stigma-peeling-0r@icloud.com>
2024-08-24 22:53:53 +0200

    ex2 reworked

--- Ex2.hs
+++ Ex2.hs
@@ -3,12 +3,15 @@ module Ex2 where
 -- I solved this by cheating. I just overshoot and take only numbers smaller than 4mil.
 -- I have to find better solution.
 
+-- UPDATE: Solved it legitimately.
+
 fibonacci :: Integer -> Integer
 fibonacci n = fibonacci' 0 1 n
   where
     fibonacci' a b 1 = b
     fibonacci' a b n = fibonacci' b (a+b) (n-1)
 
-solution :: Integer -> Integer
-solution 0 = 0
-solution n = sum $ filter (\x -> even x && x < 4_000_000) $ map fibonacci [1..n]
+fibs = map fibonacci [1..]
+
+solution :: Integer
+solution = sum $ filter (\x -> even x) $ takeWhile (\y -> y<4_000_000) fibs
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # euler.hs
+Solving Euler exercises in Haskell to practice:
+
+- [x] Exercise 1
+- [x] Exercise 2
